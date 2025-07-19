22e11a32ca097aff0b89129d57aef5982caf6f0d
Kseniya <baharevaxen@yandex.ru>
2025-07-19 15:49:50 +0300

    correcting test_mask

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,5 +1,6 @@
 import pytest
 
+
 from src.masks import get_mask_account, get_mask_card_number
 
 
@@ -11,3 +12,16 @@ def test_get_mask_card_number(mask_card, mask_account):
 def test_get_mask_account(mask_card, mask_account):
     """Тестирование функции get_mask_account"""
     assert get_mask_account(mask_account[0]) == mask_account[1]
+
+
+@pytest.mark.parametrize("mask_card, expected", [
+    ("7000792289606361", "7000 79** **** 6361"),
+])
+def tests_multiple_get_mask_card_number(mask_card, expected):
+    assert get_mask_card_number(mask_card) == expected
+
+@pytest.mark.parametrize("mask_count, expected", [
+    ("73654108430135874305", "** 4305"),])
+
+def tests_multiple_get_mask_account(mask_count, expected):
+    assert get_mask_account(mask_count) == expected
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,5 +1,6 @@
 import pytest
 
+
 from src.widget import get_date, mask_account_card
 
 
