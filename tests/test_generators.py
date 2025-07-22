ce3732c7ad3f46a86da20fa704bff45cfbfc83fc
Kseniya <baharevaxen@yandex.ru>
2025-07-23 01:28:14 +0300

    new test 2

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -9,3 +9,14 @@ def test_filter_not_existing_currency(transactions):
     {'id': 142264268, 'state': 'EXECUTED', 'date': '2019-04-04T23:20:05.206878', 'operationAmount': {'amount': '79114.93', 'currency': {'name': 'USD', 'code': 'USD'}}, 'description': 'Перевод со счета на счет', 'from': 'Счет 19708645243227258542', 'to': 'Счет 75651667383060284188'},
     {'id': 895315941, 'state': 'EXECUTED', 'date': '2018-08-19T04:27:37.904916', 'operationAmount': {'amount': '56883.54', 'currency': {'name': 'USD', 'code': 'USD'}}, 'description': 'Перевод с карты на карту', 'from': 'Visa Classic 6831982476737658', 'to': 'Visa Platinum 8990922113665229'}]
 
+
+# def test_filter_by_currency_invalid():
+#     """Тестирует по заданной валюте"""
+#     invalid_currency = {"amount": "9824.07", "currency": {"name": "USD","code": "EUR"}}
+#     with pytest.raises(ValueError):
+#         filter_by_currency(invalid_currency)
+
+
+def test_filter_by_currency(transactions):
+    """ Тестирует когда транзакции в заданной валюте отсутствуют"""
+    assert filter_by_currency(transactions, "EUR") == []
