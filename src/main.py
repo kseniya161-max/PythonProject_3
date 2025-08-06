75575bb40f9ad0d83616dd5e14a8796ee3865043
Kseniya <baharevaxen@yandex.ru>
2025-08-06 19:16:40 +0300

    adding len operation

--- src/main.py
+++ src/main.py
@@ -9,104 +9,116 @@ def main_greating():
     greating = "Привет! Добро пожаловать в программу работы с банковскими транзакциями."
     print(greating)
 
+
 def open_json():
-    """ Функция для открытия файла json"""
     with open("../logs/operations.json", "r", encoding="utf-8") as file:
         data = json.load(file)
         for operation in data:
-            if 'date' in operation:  # Проверяем, есть ли ключ 'date'
+            if 'date' in operation:
                 try:
                     operation['date'] = datetime.strptime(operation['date'], '%Y-%m-%dT%H:%M:%S.%f')
                 except ValueError:
                     print(f"Ошибка при разборе даты: {operation['date']}")
-                    operation['date'] = None  # или установите значение по умолчанию
-            else:
-                print("Запись не содержит ключ 'date':", operation)  # Выводим сообщение для отладки
-                operation['date'] = None  # или установите значение по умолчанию
-
-
+                    operation['date'] = None
         return data
 
 
+
+
 def open_csv():
-    """Функция для открытия файла CSV"""
     with open("trans_file/transactions.csv", "r", encoding="utf-8") as file:
-        reader = csv.DictReader(file, delimiter=";")  # Используем DictReader вместо reader
-        data_json = list(reader)  # Преобразуем в список словарей
+        reader = csv.DictReader(file, delimiter=";")
+        data_json = list(reader)
         for operation in data_json:
-            operation["date"] = datetime.strptime(operation['date'], '%Y-%m-%dT%H:%M:%S.%fZ')
+            date_string = operation['date']
+            if date_string:
+                try:
+                    operation["date"] = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
+                except ValueError:
+                    print(f"Ошибка при разборе даты: {date_string}")
+                    operation["date"] = None
+            else:
+                operation["date"] = None
         return data_json
 
 
 def open_excel():
-    """ Функция для открытия файла Excel"""
     data_excel = pd.read_excel("trans_file/transactions_excel.xlsx", engine="openpyxl")
     data_records = data_excel.to_dict(orient='records')
     for operation in data_records:
-        operation['date'] = pd.to_datetime(operation['date']).to_pydatetime()
+        if pd.notnull(operation['date']):
+            operation['date'] = pd.to_datetime(operation['date']).to_pydatetime()
+        else:
+            operation['date'] = None
     return data_records
 
 
 def filtered_by_status(data, status):
-    """Функция фильтрует по статусу Нр: Executed, Canceled"""
+    # return [operation for operation in data if operation.get('state', '').strip().lower() == status]
     return [
         operation for operation in data
         if isinstance(operation.get('state'), str) and operation['state'].strip().lower() == status
     ]
 
+
 def filtered_by_date(data, ascending):
     """Функция фильтрует операции по дате."""
-    return sorted(data, key=lambda x: x.get('date') or datetime.min, reverse=not ascending)
+    return sorted(data, key=lambda x: x['date'] if isinstance(x['date'], datetime) else datetime.min, reverse=not ascending)
 
 
 def sort_currency(data, code):
     """Функция фильтрует по валюте"""
     return [operation for operation in data if operation.get("operationAmount", {}).get("currency", {}).get("code") == code]
 
+
 def filtered_by_word(data, word):
-    """Функция фильтрует по ключевому слову"""
-    # return [operation for operation in data if word.lower() in operation.get("description", "").lower()]
-    return [operation for operation in data if word.lower() in operation['description'].lower()]
+    return [operation for operation in data if word.lower() in operation.get('description', '').lower()]
+
 
 def main_user_input():
-    users_input = input("Выберите необходимый пункт меню:\n"
+    while True:
+        users_input = input("Выберите необходимый пункт меню:\n"
           "1. Получить информацию о транзакциях из JSON-файла \n"
           "2. Получить информацию о транзакциях из CSV-файла \n"
           "3. Получить информацию о транзакциях из XLSX-файла \n")
 
-    if users_input == "1":
-                return "Для обработки выбран JSON-файл", open_json()
-    elif users_input == "2":
+        if users_input == "1":
+            return "Для обработки выбран JSON-файл", open_json()
+        elif users_input == "2":
             return "Для обработки выбран CSV-файл", open_csv()
-    elif users_input == "3":
-        return "Для обработки выбран XLSX-файл", open_excel()
-    else:
-        return "Неверный ввод, введите 1, 2, или 3", None
+        elif users_input == "3":
+            return "Для обработки выбран XLSX-файл", open_excel()
+        else:
+            print("Неверный ввод, введите 1, 2, или 3")
 
 
 def status_input(data):
-    status = input("Введите статус, по которому необходимо выполнить фильтрацию."
-                         "EXECUTED, CANCELED, PENDING").strip().lower()
-    filtered_data = filtered_by_status(data, status)
-    if filtered_data:
-        print(f"Операции отфильтрованы по статусу \"{status}\":")
-        for operation in filtered_data:
-            print(operation)
-    else:
-        print(f"Нет операций со статусом \"{status}\". Введите статус, по которому необходимо выполнить фильтрацию. Доступные для фильтровки статусы: EXECUTED, CANCELED, PENDING")
+    valid_statuses = ['executed', 'canceled', 'pending']
+    while True:
+        status = input("Введите статус, по которому необходимо выполнить фильтрацию." "EXECUTED, CANCELED, PENDING\n").strip().lower()
+        if status in valid_statuses:
+            filtered_data = filtered_by_status(data, status)
+            print(f"Операции отфильтрованы по статусу \"{status}\":")
+            for operation in filtered_data:
+                print(operation)
+            return filtered_data
+        else:
+            print(f"Статус операции \"{status}\" недоступен.")
 
 
 def curr_input(data):
-    code = input("Выводить только рублевые транзакции? Да/Нет").strip().lower()
+    code = input("Выводить только рублевые транзакции? Да/Нет\n").strip().lower()
     if code.strip().lower() == "да":
         filtered_code = sort_currency(data, "RUB")
         print("Отфильтрованные транзакции:")
+        if filtered_code:
+            for operation in filtered_code:
+                print(operation)
+            print(f"Всего банковских операций в выборке: {len(filtered_code)}")
+        else:
+            print("Не найдено ни одной транзакции, подходящей под Ваши условия фильтрации.")
 
-        for operation in filtered_code:
-            print(operation)
-        print(f"Всего банковских операций в выборке: {len(filtered_code)}")
-
-        user_input_2 = input("Отфильтровать список транзакций по определенному слову в описании? Да/Нет").strip().lower()
+        user_input_2 = input("Отфильтровать список транзакций по определенному слову в описании? Да/Нет\n").strip().lower()
         if user_input_2 == "да":
             word = input("Введите слово, по которому нужно отфильтровать: ")
             filtered_word = filtered_by_word(filtered_code, word)
@@ -119,11 +131,10 @@ def curr_input(data):
                 print("Нет операций, соответствующих указанному слову.")
 
 
-
 def sort_operation(data):
-    user_input_sort = input ("Отсортировать операции по дате? Да/Нет")
+    user_input_sort = input ("Отсортировать операции по дате? Да/Нет\n")
     if user_input_sort.strip().lower() == "да":
-        user_input_sort_1 = input("Отсортировать по возрастанию или по убыванию? Возрастание/Убывание").strip().lower()
+        user_input_sort_1 = input("Отсортировать по возрастанию или по убыванию? Возрастание/Убывание\n").strip().lower()
         ascending = user_input_sort_1 == "возрастание"
         sorted_data = filtered_by_date(data, ascending)
         print("Отсортированные операции:")
@@ -131,6 +142,42 @@ def sort_operation(data):
             print(operation)
 
 
+def format_and_print_operations(data):
+    """Функция выводит отфильтрованные операции в корректном виде"""
+    print(f"Всего банковских операций в выборке: {len(data)}\n")
+    for operation in data:
+        if isinstance(operation["date"], datetime):
+            date_str = operation["date"].strftime("%d.%m.%Y")
+        else:
+            date_str = "Дата не указана"
+        description = operation.get('description', 'Описание не указано')
+        operation_amount = operation.get('operationAmount')
+        if operation_amount:
+            amount = operation_amount.get('amount', 'Сумма не указана')
+            currency = operation_amount.get('currency', {}).get('name', 'Валюта не указана')
+        else:
+            amount = 'Сумма не указана'
+            currency = 'Валюта не указана'
+
+        from_account = operation.get('from', 'Счет не указан')
+        to_account = operation.get('to', 'Счет не указан')
+
+        #Сама маскировка
+        if isinstance(from_account, str):
+            if "Счет" in from_account:
+                from_account = from_account.replace(from_account[6:10], "**")
+            elif  "Mastercard" in from_account or "Visa" in from_account:
+                from_account = from_account[:-4] + "** ****" + from_account[0:7]
+        if isinstance(to_account, str):
+            if "Счет" in to_account:
+                to_account = to_account.replace(to_account[6:10], '**')
+            elif 'MasterCard' in to_account or 'Visa' in to_account:
+                to_account = to_account[:-4] + '**** **** ' + to_account[0:7]
+
+
+        print(f"{date_str} {description}")
+        print(f"{from_account} -> {to_account}")
+        print(f"Сумма: {amount} {currency}\n")
 
 
 if __name__ == "__main__":
@@ -138,22 +185,10 @@ if __name__ == "__main__":
     result, data = main_user_input()
     print(result)
     if data is not None:
-        print(data)
-    status_input(data)
-    sort_operation(data)
-    curr_input(data)
-
-
-
-
-
-    # def main():
-    #     transactions = load_transactions()
-    #     status = input("Введите статус транзакций для фильтрации: ")
-    #     filtered_transactions = filter_by_status(transactions, status)
-    #     order = input("Отсортировать по возрастанию или убыванию? ")
-    #     sorted_transactions = sort_transactions(filtered_transactions, order)
-    #     print("Отфильтрованные и отсортированные транзакции:", sorted_transactions)
+        status_data = status_input(data)
+        sort_operation(status_data)
+        curr_input(status_data)
+        format_and_print_operations(status_data)
 
 
 
